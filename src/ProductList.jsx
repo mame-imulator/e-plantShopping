695bd57513fd3809ff250a8177aaf9ec24f4477e
mame-imulator <hottrash0@gmail.com>
2025-07-26 15:54:36 -0400

    o

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -262,14 +262,15 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
 
-    const handleAddToCart = (product) => { 
-        dispatch(addItem(product)); 
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
       
-        setAddedToCart((prevState) => ({ 
-          ...prevState, 
-          [product.name]: true, 
-          [product.button]: false, 
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+          ...prevState, // Spread the previous state to retain existing entries
+          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+          [product.addedToCart]: true,
         }));
+
       };
 
     return (
