fea586a8b3d127390a126ec9072d9b6ded3fbe7e
mame-imulator <hottrash0@gmail.com>
2025-07-26 12:49:06 -0400

    p

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartSlice.js'
+import addItem from './CartSlice.jsx'
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+
 
     const plantsArray = [
         {
