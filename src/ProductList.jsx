b3a0baf469b5ea9bc5a849393f3bbfd3ba1c27cc
mame-imulator <hottrash0@gmail.com>
2025-07-26 12:18:35 -0400

    p

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -253,14 +253,14 @@ function ProductList({ onHomeClick }) {
         setShowCart(false);
     };
     return (
-        <div><p>xxxxxx</p>
+        <div>
             <div className="navbar" style={styleObj}>
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" onClick={(e) => handleHomeClick(e)}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
+                                <h3 style={{ color: 'white' }}>xxxParadise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
                             </div>
                         </a>
